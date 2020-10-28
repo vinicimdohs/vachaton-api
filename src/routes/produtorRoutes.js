40bbc0bf8245b0f5eba1cab94b46fc86ffbcb959
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2020-10-27 21:38:28 -0300

    criando middleware de verificação

--- src/routes/produtorRoutes.js
+++ src/routes/produtorRoutes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import produtorController from '../controllers/ProdutorController'
+import produtorController from '../controllers/ProdutorController';
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router;
 
 router.post('/',produtorController.store);
-router.get('/',produtorController.index);
+router.get('/',loginRequired,produtorController.index);
 router.get('/:id',produtorController.show);
 router.put('/:id',produtorController.update);
 router.delete('/:id',produtorController.remove);
